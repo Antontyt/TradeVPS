f88de33ae4e07d6ecd2a725409bee98492419216
Antontyt <antontyt@gmail.com>
2022-11-23 11:42:34 +0300

    Update Search.bat

--- SearchDisable.bat
+++ SearchDisable.bat
@@ -1,1 +1,2 @@
-dism /online /disable-feature /featurename:SearchEngine-Client-Package
+REM https://serverfault.com/questions/749921/remove-reboot-request-for-dism
+dism /online /disable-feature /featurename:SearchEngine-Client-Package /NoRestart
