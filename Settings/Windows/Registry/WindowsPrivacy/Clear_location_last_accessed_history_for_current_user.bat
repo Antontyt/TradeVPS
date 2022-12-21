e56dee8d444e9f532c4df65a422c69eabf3e7367
Antontyt <antontyt@gmail.com>
2022-12-22 00:10:53 +0300

    Update Clear_location_last_accessed_history_for_current_user.bat

--- Settings/Windows/Registry/WindowsPrivacy/Clear_location_last_accessed_history_for_current_user.bat
+++ Settings/Windows/Registry/WindowsPrivacy/Clear_location_last_accessed_history_for_current_user.bat
@@ -12,4 +12,4 @@ Powershell -Command "Get-Item -Path HKCU:\Software\Microsoft\Windows\CurrentVers
 
 
 :: Clear desktop apps last accessed history
-Powershell -Command "Get-Item -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location\NonPackaged\* | Remove-Item -Recurse -Force"
+Powershell -Command "Get-Item -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location\NonPackaged\* | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue"
