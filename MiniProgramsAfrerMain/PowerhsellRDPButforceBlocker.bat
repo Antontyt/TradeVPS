0e2010b40787bc720cb6e86cdfa3a84ba9a84b50
Antontyt <antontyt@gmail.com>
2022-12-23 23:22:43 +0300

    Update MinPrograms

--- MiniProgramsAfrerMain/PowerhsellRDPButforceBlocker.bat
+++ MiniProgramsAfrerMain/PowerhsellRDPButforceBlocker.bat
@@ -1,5 +1,6 @@
-IF NOT EXIST "C:\Windows\TEMP\WindowsServerSecurity\RDPButforceBlocker\" MD C:\Windows\TEMP\WindowsServerSecurity\RDPButforceBlocker\
+IF EXIST "C:\Windows\TEMP\WindowsServerSecurity\RDPButforceBlocker\" RMDIR /S /Q "C:\Windows\TEMP\WindowsServerSecurity\RDPButforceBlocker\"
+IF NOT EXIST "C:\Windows\TEMP\WindowsServerSecurity\RDPButforceBlocker\" MD "C:\Windows\TEMP\WindowsServerSecurity\RDPButforceBlocker\"
 IF NOT EXIST "C:\Service\Logs\" MD "C:\Service\Logs\"
-"C:\Service\System\curl\curl.exe" -O --output-dir "C:\Service\Software\PowershellScripts\" "https://raw.githubusercontent.com/Antontyt/WindowsServerSecurity/main/Settings/Programs/PowershellScripts/Block_RDP_Attack.ps1"
-"C:\Service\System\curl\curl.exe" -O --output-dir "C:\Windows\TEMP\WindowsServerSecurity\RDPButforceBlocker\" "https://raw.githubusercontent.com/Antontyt/WindowsServerSecurity/main/System/wp/CreateBlock_RDP_Attack_Task.ps1"
+"C:\Service\System\curl\curl.exe" -L --output C:\Service\Software\PowershellScripts\Block_RDP_Attack.ps1 "https://raw.githubusercontent.com/Antontyt/WindowsServerSecurity/main/Settings/Programs/PowershellScripts/Block_RDP_Attack.ps1"
+"C:\Service\System\curl\curl.exe" -L --output C:\Windows\TEMP\WindowsServerSecurity\RDPButforceBlocker\CreateBlock_RDP_Attack_Task.ps1 "https://raw.githubusercontent.com/Antontyt/WindowsServerSecurity/main/System/wp/CreateBlock_RDP_Attack_Task.ps1"
 PowerShell.exe -ExecutionPolicy Bypass -File "C:\Windows\TEMP\WindowsServerSecurity\RDPButforceBlocker\CreateBlock_RDP_Attack_Task.ps1"
--- MiniProgramsAfrerMain/RDPDefenderInstall.bat
+++ MiniProgramsAfrerMain/RDPDefenderInstall.bat
@@ -1,4 +1,4 @@
-IF NOT EXIST "C:\Windows\TEMP\WindowsServerSecurity\RDPDefender\" MD C:\Windows\TEMP\WindowsServerSecurity\RDPDefender\
-"C:\Service\System\curl\curl.exe" -O --output-dir "C:\Windows\TEMP\WindowsServerSecurity\RDPDefender\" "https://github.com/Antontyt/WindowsServerSecurity/raw/main/Settings/Programs/RDPDefender/Setup-RDPDefender.exe"
+IF EXIST "C:\Windows\TEMP\WindowsServerSecurity\RDPDefender\" RMDIR /S /Q "C:\Windows\TEMP\WindowsServerSecurity\RDPDefender\"
+IF NOT EXIST "C:\Windows\TEMP\WindowsServerSecurity\RDPDefender\" MD "C:\Windows\TEMP\WindowsServerSecurity\RDPDefender\"
+"C:\Service\System\curl\curl.exe" -L --output C:\Windows\TEMP\WindowsServerSecurity\RDPDefender\Setup-RDPDefender.exe "https://github.com/Antontyt/WindowsServerSecurity/raw/main/Settings/Programs/RDPDefender/Setup-RDPDefender.exe"
 "C:\Windows\TEMP\WindowsServerSecurity\RDPDefender\Setup-RDPDefender.exe" /VERYSILENT
-PAUSE
